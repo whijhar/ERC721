0b693b1149a59eb71a71db09623a76ef8238d6f5
whijhar <98197972+whijhar@users.noreply.github.com>
2022-04-22 14:21:39 -0500

    Update SimpleNftLowerGas.sol

--- contract/SimpleNftLowerGas.sol
+++ contract/SimpleNftLowerGas.sol
@@ -1,20 +1,4 @@
 // SPDX-License-Identifier: MIT
-
-// Amended by HashLips
-/**
-    !Disclaimer!
-
-    These contracts have been used to create tutorials,
-    and was created for the purpose to teach people
-    how to create smart contracts on the blockchain.
-    please review this code on your own before using any of
-    the following code for production.
-    The developer will not be responsible or liable for all loss or 
-    damage whatsoever caused by you participating in any way in the 
-    experimental code, whether putting money into the contract or 
-    using the code for your own project.
-*/
-
 pragma solidity >=0.7.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
